6ae41631b3385a441c328fe716cc7cc25a26ee07
Chester ONeill <coneill@relaynetwork.com>
2020-11-03 11:58:30 -0500

    Updating for docker-compose examples

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [cheshire/cheshire "5.10.0"]]
+                 [cheshire/cheshire "5.10.0"]
+                 [com.taoensso/carmine "3.0.1"]]
   :main ^:skip-aot docker-tutorial-clj.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/docker_tutorial_clj/core.clj
+++ src/docker_tutorial_clj/core.clj
@@ -1,7 +1,15 @@
 (ns docker-tutorial-clj.core
-  (:require [cheshire.core :refer :all])
+  (:require [cheshire.core :refer :all]
+            [taoensso.carmine :as car :refer (wcar)])
   (:gen-class))
+
+(def host (or (. System (getenv "REDIS_HOST")) "localhost"))
+(println host)
+
+(def server1-conn {:pool {} :spec {:uri (str "redis://" host ":6379/")}})
+(defmacro wcar* [& body] `(car/wcar server1-conn ~@body))
 
 (defn -main
   [& args]
+  (println "Response from redis" (wcar* (car/get "foo")))
   (println (generate-string {:hello "world"})))
